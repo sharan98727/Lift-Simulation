df4a41379f19e86c62fdea1f005ec0636ad52507
Sharan Reddi <sharanreddi@Sharans-MacBook-Pro.local>
2023-05-01 15:08:51 +0530

    loading js code in after form submission

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,6 @@ const form = document.getElementById('lift-simulation-form');
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-    // const floors = document.getElementById('floors').value;
-    // const lifts = document.getElementById('lifts').value;
-    // console.log(floors, lifts)
-    // const url = `../liftHtml/lifts.html?floors=${encodeURIComponent(floors)}&lifts=${encodeURIComponent(lifts)}`;
-    // window.location.href = url;
-    // //const url = '../liftHtml/lifts.html'
-    // Get the form data
     let formData = new FormData(form);
 
     // Set the URL of the new page
@@ -16,7 +9,8 @@ form.addEventListener('submit',(e)=>{
 
     // Add the form data to the URL as query parameters
     url += "?" + new URLSearchParams(formData).toString();
-
+    console.log(url)
     // Navigate to the new page
     window.location.href = url;
+    return;
 })
